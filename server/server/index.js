c1e69370200b4e6715cc3fc69b59e94f830e02c2
jesangai <jagg1996.jj@gmail.com>
2019-12-02 20:59:26 -0500

    cambios database

--- server/server/index.js
+++ server/server/index.js
@@ -1,14 +1,37 @@
+'use strict'
+
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const router = express.Router();
+const propierties = require('./config/propierties');
+const DB = require('./config/db');
+//const { mongoose } = require('./config/db');
 
-const { mongoose } = require('./database');
+var path=require('path')
 
+//instancia base
+
+DB();
+
+const bodyParse=require('body-parse')
+const bodyParserURLEncoded = bodyParse.urlencoded({extended:true})
+
+app.use(bodyParse.json())
+app.use(bodyParserURLEncoded)
 // Settings
 app.set('port', process.env.PORT || 3000);
 
 // Middlewares
+app.use('/',express.static('cliente',{redirect:false}));
 app.use(cors({origin: 'http://68.66.207.7:4200'}));
+
+app.use(function (req, res, next) {
+
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.json());
 
 // Routes
@@ -22,6 +45,4 @@ app.get('*',function(req,res,next){
   });
 
 // starting the server
-app.listen(app.get('port'), () => {
-    console.log(`server on port ${app.get('port')}`);
-});
+app.listen(propierties.PORT,()=>console.log(`server runing on port ${propierties.PORT}`))
